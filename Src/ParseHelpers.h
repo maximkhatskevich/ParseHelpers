a7c9b647a9fab0ea376d8362366b09df09f5c3c5
Maxim Khatskevich <maxim.khatskevich@gmail.com>
2014-01-03 06:14:11 +0600

    Minor bug fix.

--- Src/ParseHelpers.h
+++ Src/ParseHelpers.h
@@ -17,6 +17,5 @@
 #import <ParseHelpers/NSArray+ParseHelpers.h>
 #import <ParseHelpers/NSMutableArray+ParseHelpers.h>
 #import <ParseHelpers/PFGeoPoint+Helpers.h>
-#import <ParseHelpers/GenericPFSubclass.h>
 
 #endif
